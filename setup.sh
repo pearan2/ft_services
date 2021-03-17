cf30bc9e657e26617e24a0ecd0f3a1ff24809545
Hongkwon Lee <honlee@c7r5s8.42seoul.kr>
2021-03-17 21:14:39 +0900

    grafana dashboard json remain

--- setup.sh
+++ setup.sh
@@ -14,7 +14,9 @@ docker build -t honlee_mysql srcs/mysql
 docker build -t honlee_wordpress srcs/wordpress
 docker build -t honlee_phpmyadmin srcs/phpmyadmin
 docker build -t honlee_ftps srcs/ftps
-
+docker build -t honlee_influxdb srcs/influxdb
+docker build -t honlee_telegraf srcs/telegraf
+docker build -t honlee_grafana srcs/grafana
 # 로드밸런서
 minikube addons enable metallb
 kubectl apply -f ./srcs/metallb.yaml
@@ -26,5 +28,9 @@ kubectl apply -f ./srcs/nginx.yaml
 kubectl apply -f ./srcs/phpmyadmin.yaml
 kubectl apply -f ./srcs/ftps.yaml
 kubectl apply -f ./srcs/wordpress.yaml
+kubectl apply -f ./srcs/influxdb.yaml
+kubectl apply -f ./srcs/telegraf.yaml
+kubectl apply -f ./srcs/grafana.yaml
+
 #echo "대쉬보드를 실행합니다."
 #minikube dashboard > /dev/null
--- srcs/mysql/mysql_setup.sh
+++ srcs/mysql/mysql_setup.sh
@@ -6,5 +6,6 @@ mysql_install_db --user=root
 
 #bootstrap 옵션을 사용하는 이유는 mysql 서버가 시작 되기전에 먼저 db테이블이 만들어져야 하기 때문.
 mysqld -u root --bootstrap < mysql_init.sql
+#mysqld -u root --bootstrap < wordpress.sql
 #진짜 서버 시작.
 mysqld -u root

0c2230d7d7331a5185c0e1737ba9d9a0b792819d
Hongkwon Lee <honlee@c7r5s8.42seoul.kr>
2021-03-18 01:15:06 +0900

    solved

--- srcs/nginx/nginx_setup.sh
+++ srcs/nginx/nginx_setup.sh
@@ -9,4 +9,6 @@ mv localhost-nginx.crt /etc/nginx/ssl
 mkdir -p /run/nginx
 echo "<h1>THIS IS FT_SERVICES NGINX INDEX.HTML BY HONLEE</h1>" >> /var/www/index.html
 
+#nginx 를 foreground 에서 실행하기 위함.
+#이렇게 실행하지 않으면 background 에서 실행하게 되고, 그럼 컨테이너가 종료된다
 /usr/sbin/nginx -g "daemon off;"
